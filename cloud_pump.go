db0018f8f29e656a975a25c8907ea32fd7ca12ec
Greg Zapp <greg.zapp@gmail.com>
2014-06-06 12:38:34 +1200

    Split graphite stuff into seperate file

--- cloud_pump.go
+++ cloud_pump.go
@@ -7,7 +7,6 @@ import (
 	"github.com/crowdmob/goamz/cloudwatch"
 	_ "github.com/davecgh/go-spew/spew"
 	"github.com/kr/pretty"
-	"github.com/marpaia/graphite-golang"
 	"github.com/pmylund/sortutil"
 	"os"
 	"regexp"
@@ -193,51 +192,4 @@ func (cwMetric *CloudWatchMetric) GetCustomKey() string {
 		cwMetric.CustomKey = StringFormatExp.ReplaceAllStringFunc(cwMetric.CustomKeyFormat, replacer)
 	}
 	return cwMetric.CustomKey
-}
-
-func CloudPumpResultToMetrics(in <-chan *MonResult, out chan *graphite.Metric) {
-	for res := range in {
-		for _, metric := range res.Resp.GetMetricStatisticsResult.Datapoints {
-			graphiteMetric := &graphite.Metric{
-				res.CwMetric.GetCustomKey(), strconv.FormatFloat(metric.Sum, 'f', 5, 64), metric.Timestamp.Unix(),
-			}
-			fmt.Printf("%+v\n", graphiteMetric)
-		}
-	}
-}
-
-func MetricsToGraphite(c <-chan *graphite.Metric, host string, port int) {
-	//Must be able to connect to graphite first or conn will be nill
-	//Con is not exported so we can't check it later
-	var Graphite *graphite.Graphite
-	for {
-		Graphite = &graphite.Graphite{Host: host, Port: port}
-		err := Graphite.Connect()
-		if err != nil {
-			fmt.Println("Unable to connect to graphite")
-			time.Sleep(2 * time.Second)
-			continue
-		}
-		break
-	}
-	for metric := range c {
-		for {
-			fmt.Println("Sending metric: ", metric)
-			err := Graphite.SendMetric(*metric)
-			if err != nil {
-				//Sending the metric has failed, likely due to a connection issue
-				//Attempt to reconnect and then restart sending this result
-				fmt.Println("Sending failed, attempting to reconnect")
-				for {
-					if err := Graphite.Connect(); err == nil {
-						break
-					}
-					fmt.Println("Graphite reconnnect failed: ", err.Error())
-					time.Sleep(5 * time.Second)
-				}
-				continue
-			}
-			break
-		}
-	}
 }
---
+++ graphite.go
@@ -0,0 +1,55 @@
+package cloudpump
+
+import (
+	"fmt"
+	"github.com/marpaia/graphite-golang"
+	"strconv"
+	"time"
+)
+
+func CloudPumpResultToMetrics(in <-chan *MonResult, out chan *graphite.Metric) {
+	for res := range in {
+		for _, metric := range res.Resp.GetMetricStatisticsResult.Datapoints {
+			graphiteMetric := &graphite.Metric{
+				res.CwMetric.GetCustomKey(), strconv.FormatFloat(metric.Sum, 'f', 5, 64), metric.Timestamp.Unix(),
+			}
+			fmt.Printf("%+v\n", graphiteMetric)
+		}
+	}
+}
+
+func MetricsToGraphite(c <-chan *graphite.Metric, host string, port int) {
+	//Must be able to connect to graphite first or conn will be nill
+	//Con is not exported so we can't check it later
+	var Graphite *graphite.Graphite
+	for {
+		Graphite = &graphite.Graphite{Host: host, Port: port}
+		err := Graphite.Connect()
+		if err != nil {
+			fmt.Println("Unable to connect to graphite")
+			time.Sleep(2 * time.Second)
+			continue
+		}
+		break
+	}
+	for metric := range c {
+		for {
+			fmt.Println("Sending metric: ", metric)
+			err := Graphite.SendMetric(*metric)
+			if err != nil {
+				//Sending the metric has failed, likely due to a connection issue
+				//Attempt to reconnect and then restart sending this result
+				fmt.Println("Sending failed, attempting to reconnect")
+				for {
+					if err := Graphite.Connect(); err == nil {
+						break
+					}
+					fmt.Println("Graphite reconnnect failed: ", err.Error())
+					time.Sleep(5 * time.Second)
+				}
+				continue
+			}
+			break
+		}
+	}
+}
